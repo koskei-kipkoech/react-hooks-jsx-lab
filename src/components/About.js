be36a3252e6ed290d5095ed6ede3378ebde707a8
koskei-kipkoech <patrickwayy@gmail.com>
2024-10-30 08:50:24 +0300

    updates on src/components

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { image } from "../data/data";
-
+import "../index.css"
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Hello this is the design of kipp</p>
+    <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
